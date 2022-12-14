834127326174e9ddaf99674e60eb1907f5b78a40
HyeonJong Kim <ujiko2003@gmail.com>
2022-12-14 18:42:10 +0900

    Initial commit

---
+++ src/test/java/com/example/Spring_boot_Studying/SpringBootStudyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_boot_Studying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
