9204b12371460e992beef1e2f374a2ec63553627
HyeonJong Kim <ujiko2003@gmail.com>
2022-12-17 15:23:30 +0900

    DI 3가지 방법

--- src/main/java/com/example/Spring_boot_Studying/Controller/MemberController.java
+++ src/main/java/com/example/Spring_boot_Studying/Controller/MemberController.java
@@ -7,8 +7,19 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
 
-    private final MemberService memberService;
+    //1.DI 중 필드 주입 //그리 좋은 방법은 아니다.
+    //@Autowired private MemberService memberService;
 
+    private  MemberService memberService;
+
+
+    //2.DI 중 Setter 주입
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+
+    //3.DI 중 생성자 주입 방법 //생성자 주입이 가장 좋다.
     @Autowired //서비스와 컨트롤러 연결
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
